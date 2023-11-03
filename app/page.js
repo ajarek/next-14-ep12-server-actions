89fbe8a1cf34d524eae6c76d35672b92f1d1b9be
Ajarek <ajarek@poczta.onet.pl>
2023-11-03 01:03:29 +0100

    added shadcn/ui

--- app/page.js
+++ app/page.js
@@ -1,47 +1,90 @@
 import { revalidatePath } from 'next/cache'
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+import {
+  useFormField,
+  Form,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormDescription,
+  FormMessage,
+  FormField, } from "@/components/ui/form"
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+import { ProfileForm } from '@/components/ui/ProfileForm'
 
 
 let productsStore=[
   {id:'1',
-  title:"czajnik"
+  title:"czajnik",
+  price:"58.00"
 },
   {id:'2',
-  title:"garnek"
+  title:"garnek",
+  price:"158.00"
 }
   
 ]
 
 export default function Home() {
+
+  
   
     async function myAction(formData) {
-      
       'use server'
+      console.log(formData);
       productsStore=[{
         id:Math.floor(Math.random()*1000),
-        title:formData.get('title')
+        title:formData.get('title'),
+        price:formData.get('price')
       },
       ...productsStore
     ]
-
+    
     revalidatePath('/')
     
     }
+
+
   
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <form action={myAction}>
-      <input type="text" name="title" />
-      <button type="submit">Add Product</button>
-    </form>
-    <div>
-      {productsStore.map(el=>{
-        return(
-          <div key={el.id}>
-            <p>{el.title}</p>
-          </div>
-        )
-      })}
-    </div>
+    <main className="flex min-h-screen flex-col items-center justify-start px-24">
+      {/* <form action={myAction} className='flex flex-col gap-2 '>
+      <Input type="text" name="title" placeholder='' />
+      <Input type="text" name="price" placeholder='podaj cenę artykułu' />
+      <Button variant="default" type="submit">Dodaj product</Button>
+    </form> */}
+    <ProfileForm action={myAction}/>
+    <Table className='border-2 border-slate-600 mt-4'>
+  <TableCaption>Lista dodanych towarów </TableCaption>
+  <TableHeader >
+    <TableRow>
+      <TableHead className="w-[100px] font-bold">Nr</TableHead>
+      <TableHead className='font-bold'>Status Towaru</TableHead>
+      <TableHead className='font-bold'>Nazwa Towaru</TableHead>
+      <TableHead className="text-right font-bold">Cena w PLN</TableHead>
+    </TableRow>
+  </TableHeader>
+  <TableBody>
+  {productsStore.map(el=>
+    <TableRow key={el.id} className='border-2 border-slate-600'>
+      <TableCell className="font-medium">{el.id}</TableCell>
+      <TableCell>Dostępny</TableCell>
+      <TableCell>{el.title}</TableCell>
+      <TableCell className="text-right">{el.price}</TableCell>
+    </TableRow>
+     )}
+  </TableBody>
+</Table>
+
     </main>
   )
 }
---
+++ components/ui/ProfileForm.jsx
@@ -0,0 +1,62 @@
+"use client"
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import * as z from "zod"
+
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+
+const formSchema = z.object({
+  title: z.string().min(2, {message: "title must be at least 2 characters.",}),
+  price: z.string().min(2, {message: "price must be at least 2 characters.",}),
+})
+
+export function ProfileForm({action}) {
+  const form = useForm()
+
+  return (
+    <Form {...form}>
+      <form action={action} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="title"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Nazwa</FormLabel>
+              <FormControl>
+                <Input placeholder="podaj nazwę artykułu" {...field} />
+              </FormControl>
+              <FormDescription/>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="price"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Cena</FormLabel>
+              <FormControl>
+                <Input placeholder="podaj cenę artykułu" {...field} />
+              </FormControl>
+              <FormDescription/>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Dodaj</Button>
+      </form>
+    </Form>
+  )
+}
